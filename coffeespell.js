1da4bc36618af250a7c6ac7ed8af25954cb842f2
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2022-05-01 11:31:53 +0900

    [mod] coffeespell.js 파일출력 기능 추가

--- coffeespell.js
+++ coffeespell.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import cheerio from "cheerio";
+import save from "./handleFile.js";
 import inspectTitle from "./util.js";
 
 const STORENAME = 'COFFEESPELL'
@@ -32,7 +33,7 @@ async function getItemList(section, url) {
         item.set('grade', grade)
 
         item.set('canBuy', $(article).find('span._3Btky8fCyp').length > 0 ? "N" : "Y")
-        item.set('detail', $(article).find('p.vChbm1yu9U').text() || '')
+        item.set('detail', ($(article).find('p.vChbm1yu9U').text() || '').replace(',', ' '))
         item.set('extra', inspectTitle(prodName))
         itemList.push(item)
     }
@@ -57,17 +58,19 @@ async function init() {
     let obj = new Map()
     obj.set("storename", STORENAME)
     let now = new Date();
-    obj.set("today", now.getFullYear + "." + (now.getMonth < 9 ? "0" : "") + now.getMonth + 1 + "." + now.getDate)
+    obj.set("today", now.getFullYear() + "." + (now.getMonth() < 9 ? "0" : "") + (now.getMonth() + 1) + "." + (now.getDate() < 10 ? "0" + now.getDate() : now.getDate()))
     obj.set("url", 'https://smartstore.naver.com/coffeespell')
 
-    let itemList = []
     let 생두pages = await getPageList('https://smartstore.naver.com/coffeespell/category/661f22e827554c198b452d437c66dc5e')
     for(let page of 생두pages) {
-        
-        itemList.push(...await getItemList('생두', page))
+        save(obj, await getItemList('생두', page))
     }
-    console.log(itemList)
-    // let 원두list = await getPageList('https://smartstore.naver.com/coffeespell/category/ff37e49dadcc4f17b1148766d247b2bb')
+
+    let 원두pages = await getPageList('https://smartstore.naver.com/coffeespell/category/ff37e49dadcc4f17b1148766d247b2bb')
+    for(let page of 원두pages) {
+        save(obj, await getItemList('원두', page))
+    }
+
 }
 
 init();
